7e5b02a5f208e168fef3957007b8a451b38997d2
Purva kiran powar patil <109520470+Anupowarpatil@users.noreply.github.com>
2024-04-15 16:41:19 +0530

    Add files via upload

---
+++ in_private_public.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class person{
+	private:
+		char name[10];
+		int age;
+		int phone;
+		char address[20];
+};
+
+class student : public person{
+		int rollno;
+		int marks;
+		public: void getdata()
+		{
+			cout<<"Enter your name:";
+			cin>>name;
+			cout<<"Enter your age:";
+			cin>>age;
+			cout<<"Enter your phone number:";
+			cin>>phone;
+			cout<<"Enter your address:";
+			cin>>address;
+			cout<<"Enter your roll number:";
+			cin>>rollno;
+			cout<<"Enter your marks:";
+			cin>>marks;
+		}	
+		
+		public: void display()
+		{
+			cout<<name<<" "<<age<<" "<<phone<<" "<<address<<" "<<rollno<<" "<<marks;
+		}
+};
+/*
+class employee : public person{
+		int id;
+		int salary;
+};
+*/
+int main()
+{
+	student st;
+	st.getdata();
+	st.display();
+}
